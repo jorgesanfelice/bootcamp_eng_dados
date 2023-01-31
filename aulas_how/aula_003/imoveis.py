18248c07dc87119c38943a9b9038f0b2c4c19154
Jorge Sanfelice <jorgesanfelice>
2023-01-31 00:46:05 -0300

    [aula3] - atualizacao imoveis

--- aulas_how/aula_003/imoveis.py
+++ aulas_how/aula_003/imoveis.py
@@ -1,3 +1,6 @@
+
+# %%
+
 from os import WCONTINUED, link, sep
 from typing import AsyncIterable
 import requests
@@ -38,67 +41,138 @@ df = pd.DataFrame(
         'wlink'
     ]
 )
+# %%
+json_data = get_json(reqUrl,50,headersList, payload)
+
+#carregando o json de uma forma que fique visualmente facil de entender a estrutura do arquivo.
+json_formatted_str = json.dumps(json_data, indent=2)
+print(json_formatted_str)
 
 # %%
 #Inicar com o zero para começar pegando o primeiro imovel
 imovel_id = 0 
 json_data = get_json(reqUrl,imovel_id,headersList, payload)
-while len(json_data['search']['result']['listing']) > 0:
-    qnt = len(json_data['search']['result']['listing'] )
+while len(json_data['search']['result']['listings']) > 0:
+    qnt = len(json_data['search']['result']['listings'] )
     print(f'Quantidade de imóveis:{qnt} | total de imóveis: {imovel_id}')
-    
+
+    for i in range(0,qnt):
+        try:
+            descricao = json_data['search']['result']['listings'][i]['listing']['title']
+        except:
+            descricao = '-'
+        try:
+            try:
+                endereco = json_data['search']['result']['listings'][i]['listing']['adress']['street']+", "+json_data['search']['result']['listings'][i]['listing']['adress']['street']['streetNumber']
+            except:
+                endereco = json_data['search']['result']['listings'][i]['listing']['adress']['street']
+        except:
+            endereco = '-'
+        try:
+            area = json_data['search']['result']['listings'][i]['listing']['totalAreas']
+        except:
+            area = '-'
+        try:
+            quartos = json_data['search']['result']['listings'][i]['listing']['bedrooms']
+        except:
+            quartos = '-'
+        try:
+            suites = json_data['search']['result']['listings'][i]['listing']['suites']
+        except:
+            suites = '-'                    
+        try:
+            wc = json_data['search']['result']['listings'][i]['listing']['bethrooms']
+        except:
+            wc = '-'        
+        try:
+            valor = json_data['search']['result']['listings'][i]['listing']['pricingInfos'][0]['price']
+        except:
+            valor = '-'  
+        try:
+            vagas = json_data['search']['result']['listings'][i]['listing']['parkingSpaces']
+        except:
+            vagas = '-'  
+        try:
+            condominio = json_data['search']['result']['listings'][i]['listing']['pricingInfos'][0]['monthlyCondoFee']
+        except:
+            condominio = '-'  
+        try:
+            wlink = 'https://www.vivareal.com.br' + json_data['search']['result']['listings'][i]['link']['href']
+        except:
+            wlink = '-'  
+                                                
+        if endereco != '-':        
+            print(f'Descricao:{descricao}')
+            print('/n')
+            print(f'Endereço:{endereco}')              
+
+        #carregar no dataframe a linha extraida
+        df.loc[df.shape[0]] = [
+            descricao,
+            endereco,
+            area,
+            quartos,
+            wc,
+            vagas,
+            valor,
+            condominio,
+            wlink
+        ]
+        
     imovel_id = imovel_id + qnt
-    time.sleep(1)
+    if imovel_id > 10000:
+        break
+    time.sleep(1) #deixar 3 min para evitar erro de muitas requisições (barrar IP erro 429)
     json_data = get_json(reqUrl,imovel_id,headersList, payload)
 
 
 #%%
-while len(json_data['search']['result']['listing'] > 0):
-    i += 1
-    print(f"valor i: {i} \t\t qtd_imoveis: {df.shape[0]}")
-    ret = requests.get(url.format(i))
-    soup = bs(ret.text)
-    houses = soup.find_all(
-        'a', {'class': 'property-card__content-link js-card-title'})
-
-    for house in houses:
-        try:
-            descricao = house.find('span', {'class': 'property-card__title'}).text.strip()
-        except:
-            descricao = None
-        try:
-            endereco = house.find('span', {'class': 'property-card__address'}).text.strip()
-        except:
-            endereco = None
-        try:
-            area = house.find('span', {'class': 'js-property-card-detail-area'}).text.strip()
-        except:
-            area = None
-        try:
-            quartos = house.find('li', {'class': 'property-card__detail-room'}).span.text.strip()
-        except:
-            quartos = None
-        try:
-            wc = house.find('li', {'class': 'property-card__detail-bathroom'}).span.text.strip()
-        except:
-            wc = None
-        try:
-            vagas = house.find('li', {'class': 'property-card__detail-garage'}).span.text.strip()
-        except:
-            vagas = None
-        try:
-            valor = house.find('div', {'class': 'property-card__price'}).p.text.strip()
-        except:
-            valor = None
-        try:
-            condominio = house.find('strong', {'class': 'js-condo-price'}).text.strip()
-        except:
-            condominio = None
-        try:
-            wlink = 'https://www.vivareal.com.br' + house['href']
-        except:
-            wlink = None
-
+#while len(json_data['search']['result']['listing'] > 0):
+#    i += 1
+#    print(f"valor i: {i} \t\t qtd_imoveis: {df.shape[0]}")#
+#    ret = requests.get(url.format(i))#
+#    soup = bs(ret.text)#
+#    houses = soup.find_all(#
+#        'a', {'class': 'property-card__content-link js-card-title'})#
+#
+#    for house in houses:
+#        try:
+#            descricao = house.find('span', {'class': 'property-card__title'}).text.strip()
+#        except:
+#            descricao = None
+#        try:
+#            endereco = house.find('span', {'class': 'property-card__address'}).text.strip()
+#        except:
+#            endereco = None
+#        try:
+#            area = house.find('span', {'class': 'js-property-card-detail-area'}).text.strip()
+#        except:
+#            area = None
+#        try:
+#            quartos = house.find('li', {'class': 'property-card__detail-room'}).span.text.strip()
+#        except:
+#            quartos = None
+#        try:
+#            wc = house.find('li', {'class': 'property-card__detail-bathroom'}).span.text.strip()
+#        except:
+#            wc = None
+#        try:
+#            vagas = house.find('li', {'class': 'property-card__detail-garage'}).span.text.strip()
+#        except:
+#            vagas = None
+#        try:
+#            valor = house.find('div', {'class': 'property-card__price'}).p.text.strip()
+#        except:
+#            valor = None
+#        try:
+#            condominio = house.find('strong', {'class': 'js-condo-price'}).text.strip()
+#        except:
+#            condominio = None
+#        try:
+#            wlink = 'https://www.vivareal.com.br' + house['href']
+#        except:
+#            wlink = None
+#
         df.loc[df.shape[0]] = [
             descricao,
             endereco,
@@ -123,7 +197,6 @@ print(vagas)
 print(valor)
 print(condominio)
 print(wlink)
+
 #%%
-
-
 df.to_csv('banco_de_imoveis.csv', sep=';', index=False)
